d8b7648f7e1d057ed22e963dab70c5bf1963cc70
IamAmitKumarSarkar <amitsarkar132004@gmail.com>
2025-11-11 22:41:18 +0530

    add changes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import express from "express"
+
+
+
 import cors from "cors"
 import cookieParser from "cookie-parser";
 
@@ -13,7 +16,7 @@ app.use(cors({
 
 }))
 
-
+//handling data 
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
