0ddd5cbdb825e2ef4345cc9be4e39f0c9e5f770e
IamAmitKumarSarkar <amitsarkar132004@gmail.com>
2025-11-11 17:27:06 +0530

    connect database

---
+++ src/db/index.js
@@ -0,0 +1,24 @@
+//we connect database through mongoose
+import mongoose from "mongoose";
+//import DB name
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+      try{
+         const connectionInstance = await mongoose.connect
+         (`${process.env.MONGODB_URI}/${DB_NAME}`);
+        //  app.on("errror",(error) => {
+        //     console.log("ERROR: ", error);
+        //     throw error   
+        //  })
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        
+    }catch(error){
+        console.log("MongoDB connection error : ",error);
+        process.exit(1)
+    }
+}
+
+
+export default connectDB;
