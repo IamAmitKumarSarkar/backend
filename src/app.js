54c92f7a69120891ce8202c70464b3cc7f47e34b
IamAmitKumarSarkar <amitsarkar132004@gmail.com>
2025-11-13 10:01:51 +0530

    controller add

--- src/app.js
+++ src/app.js
@@ -26,4 +26,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+
+
+
+//route import 
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users",userRouter)    //we can not use app.get() because here we use a middleware router to reach user.routes.js
+
 export { app }
