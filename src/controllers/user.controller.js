cbd1f9f866058a7e29bf60f8de8b41b91110f53b
IamAmitKumarSarkar <amitsarkar132004@gmail.com>
2025-11-13 13:05:57 +0530

    Debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,7 +26,7 @@ const registerUser = asyncHandler( async (req,res) => {
         }
 
  //check user exist or not
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username },{ email }]    //email OR username any one is exist
     })
 
